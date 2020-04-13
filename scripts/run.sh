8b19bf21d2f55c43755b13b8c19a489d4992f22f
gitadrian <adrian.castro@softtek.com>
2020-04-13 14:54:48 -0700

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
-echo $(pwd)
-sudo chmod 777 ../target/devops-practice-0.0.1-SNAPSHOT.jar
-java -jar ../target/devops-practice-0.0.1-SNAPSHOT.jar
+cd /home/ec2-user
+sudo chmod 777 devops-practice-0.0.1-SNAPSHOT.jar
+sudo java -jar devops-practice-0.0.1-SNAPSHOT.jar
